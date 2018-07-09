5074beb8c0bb31cd0b0fefb097ef05ee28426beb
techinnovator <techinnovator01@gmail.com>
2018-07-09 12:14:41 -0700

    Added delegate and datasource methods to DocumentsViewController

--- Documents Core Data/DocumentsViewController.swift
+++ Documents Core Data/DocumentsViewController.swift
@@ -8,12 +8,36 @@
 
 import UIKit
 
-class DocumentsViewController: UIViewController {
+class DocumentsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let dateFormatter = DateFormatter()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .medium
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "documentCell", for: indexPath)
+        
+        if let cell = cell as? DocumentTableViewCell {
+            //let document = documents[indexPath.row]
+            //cell.nameLabel.text = document.name
+            //cell.sizeLabel.text = String(document.size))
+            //cell.modifiedLabel.text = dateFormatter.string(from: document.modifiedDate)
+        }
+        
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +45,5 @@ class DocumentsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
